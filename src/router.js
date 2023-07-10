6316201e41fa95e1a559b9c9538475bb34580570
winner <viandmarket@outlook.com>
2023-07-11 04:19:07 +0800

    m

--- src/router.js
+++ src/router.js
@@ -3,9 +3,15 @@ import ChatView from './views/chat.vue'; // Replace with your actual component
 import MarketingView from './views/marketing.vue'
 import JoinView from './views/join.vue'
 import PrivacyView from './views/privacy.vue'
+import WaitlistView from './views/waitlist.vue'
 
 
 const routes = [
+  {
+    path: '/waitlist',
+    name: 'WaitlistView',
+    component: WaitlistView,
+  },
   {
     path: '/privacy',
     name: 'PrivacyView',
---
+++ src/views/waitlist.vue
@@ -0,0 +1,133 @@
+<template>
+    <div style="background:#fbfbfb; " class="bg-white">
+   
+   <!-- ========== HEADER ========== -->
+     <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white border-b border-gray-200 text-sm py-4 dark:bg-gray-800 dark:border-gray-700">
+       <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8" aria-label="Global">
+         <div class="flex items-center justify-between">
+           <a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">SalesBoyi</a>
+           <div class="sm:hidden">
+             <button type="button" class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800" data-hs-collapse="#navbar-collapse-with-animation" aria-controls="navbar-collapse-with-animation" aria-label="Toggle navigation">
+               <svg class="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+               </svg>
+               <svg class="hs-collapse-open:block hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+               </svg>
+             </button>
+           </div>
+         </div>
+         <div id="navbar-collapse-with-animation" class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block">
+           <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
+             <a class="font-medium text-blue-500" href="#" aria-current="page">Landing</a>
+             <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">About</a>
+             <a class="font-medium text-gray-600 hover:text-gray-400 dark:text-gray-400 dark:hover:text-gray-500" href="#">Blog</a>
+             <a href="/join" style="background:#202020; height:38px; " class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white py-3 px-4 dark:focus:ring-offset-gray-800" >
+               Join us
+               <svg class="w-3 h-3" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                 <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+               </svg>
+             </a>
+           </div>
+         </div>
+       </nav>
+     </header>
+     <!-- ========== END HEADER ========== -->
+   
+     <!-- ========== MAIN CONTENT ========== -->
+     <main id="content" role="main" class="shrink-0">
+
+        <!-- Hero -->
+<div class="overflow-hidden">
+  <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-20">
+    <div class="relative mx-auto max-w-4xl grid space-y-5 sm:space-y-10">
+      <!-- Title -->
+      <div class="text-center">
+        <p class="text-xs font-semibold text-gray-500 tracking-wide uppercase mb-3 dark:text-gray-200">
+            Successfullly Joined the Waitlist!
+           
+        </p>
+        <h1 style="font-size:18px;" class="text-1xl text-gray-800 font-bold sm:text-3xl lg:text-6xl lg:leading-tight dark:text-gray-200">
+            Your Position #200
+     
+        </h1>
+        <br/>
+        <div class="flex justify-center">
+      <a class="inline-flex items-center gap-x-2 bg-white border border-gray-200 text-sm text-gray-800 p-1 pl-3 rounded-full transition hover:border-gray-300 dark:bg-gray-800 dark:border-gray-700 dark:hover:border-gray-600 dark:text-gray-200" href="/join">
+        Share the love with friends - copy link
+        <span class="py-2 px-3 inline-flex justify-center items-center gap-x-2 rounded-full bg-gray-200 font-semibold text-sm text-gray-600 dark:bg-gray-700 dark:text-gray-400">
+          <svg class="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
+            <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        </span>
+      </a>
+    </div>
+      </div>
+      <!-- End Title -->
+
+      <!-- Avatar Group -->
+      <div class="sm:flex sm:justify-center sm:items-center text-center sm:text-left">
+        <div class="flex-shrink-0 pb-5 sm:flex sm:pb-0 sm:pr-5">
+          <!-- Avatar Group -->
+          <div class="flex justify-center -space-x-3">
+          <!-- Icon -->
+                <span style="background:#202020;" class="m-1 inline-flex justify-center items-center w-[46px] h-[46px] rounded-md bg-blue-600 text-white">
+                <svg class="w-5 h-5" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19.1042 5.95502C19.2066 5.85057 19.3288 5.76759 19.4636 5.71095C19.5984 5.6543 19.7432 5.62512 19.8895 5.62512C20.0357 5.62512 20.1805 5.6543 20.3153 5.71095C20.4502 5.76759 20.5723 5.85057 20.6747 5.95502C21.1037 6.38852 21.1097 7.08902 20.6897 7.53002L11.8202 18.015C11.7195 18.1256 11.5973 18.2145 11.4611 18.2762C11.3249 18.3379 11.1775 18.3712 11.0279 18.374C10.8784 18.3768 10.7299 18.3491 10.5914 18.2925C10.453 18.236 10.3275 18.1517 10.2227 18.045L4.82571 12.576C4.61757 12.3638 4.50098 12.0783 4.50098 11.781C4.50098 11.4837 4.61757 11.1983 4.82571 10.986C4.92808 10.8816 5.05026 10.7986 5.1851 10.7419C5.31993 10.6853 5.46471 10.6561 5.61096 10.6561C5.75722 10.6561 5.902 10.6853 6.03683 10.7419C6.17167 10.7986 6.29385 10.8816 6.39621 10.986L10.9742 15.6255L19.0742 5.98802C19.0835 5.97643 19.0936 5.96541 19.1042 5.95502Z" fill="currentColor"/>
+                </svg>
+                </span>
+                <!-- End Icon -->
+
+           
+<!-- End Icon -->
+          </div>
+          <!-- End Avatar Group -->
+        </div>
+
+        <div class="border-t sm:border-t-0 sm:border-l border-gray-200 w-32 h-px sm:w-auto sm:h-full mx-auto sm:mx-0"></div>
+
+        <div class="pt-5 sm:pt-0 sm:pl-5">
+          <div class="text-lg font-semibold text-gray-800 dark:text-gray-200">People in Waitlist</div>
+          <div class="text-sm text-gray-500">3,100 </div>
+        </div>
+      </div>
+      <!-- End Avatar Group -->
+
+   
+
+      <!-- SVG Element -->
+      <div class="hidden absolute top-2/4 right-0 transform -translate-y-2/4 translate-x-40 md:block lg:translate-x-80" aria-hidden="true">
+        <svg class="w-72 h-auto" width="1115" height="636" viewBox="0 0 1115 636" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.990203 279.321C-1.11035 287.334 3.68307 295.534 11.6966 297.634L142.285 331.865C150.298 333.965 158.497 329.172 160.598 321.158C162.699 313.145 157.905 304.946 149.892 302.845L33.8132 272.418L64.2403 156.339C66.3409 148.326 61.5475 140.127 53.5339 138.026C45.5204 135.926 37.3213 140.719 35.2207 148.733L0.990203 279.321ZM424.31 252.289C431.581 256.26 440.694 253.585 444.664 246.314C448.635 239.044 445.961 229.931 438.69 225.96L424.31 252.289ZM23.0706 296.074C72.7581 267.025 123.056 230.059 187.043 212.864C249.583 196.057 325.63 198.393 424.31 252.289L438.69 225.96C333.77 168.656 249.817 164.929 179.257 183.892C110.144 202.465 54.2419 243.099 7.92943 270.175L23.0706 296.074Z" fill="currentColor" class="fill-orange-500"/>
+          <path d="M451.609 382.417C446.219 388.708 446.95 398.178 453.241 403.567L555.763 491.398C562.054 496.788 571.524 496.057 576.913 489.766C582.303 483.474 581.572 474.005 575.281 468.615L484.15 390.544L562.222 299.413C567.612 293.122 566.881 283.652 560.59 278.263C554.299 272.873 544.829 273.604 539.44 279.895L451.609 382.417ZM837.202 559.655C841.706 566.608 850.994 568.593 857.947 564.09C864.9 559.586 866.885 550.298 862.381 543.345L837.202 559.655ZM464.154 407.131C508.387 403.718 570.802 395.25 638.136 410.928C704.591 426.401 776.318 465.66 837.202 559.655L862.381 543.345C797.144 442.631 718.724 398.89 644.939 381.709C572.033 364.734 504.114 373.958 461.846 377.22L464.154 407.131Z" fill="currentColor" class="fill-cyan-600"/>
+          <path d="M447.448 0.194357C439.203 -0.605554 431.87 5.43034 431.07 13.6759L418.035 148.045C417.235 156.291 423.271 163.623 431.516 164.423C439.762 165.223 447.095 159.187 447.895 150.942L459.482 31.5025L578.921 43.0895C587.166 43.8894 594.499 37.8535 595.299 29.6079C596.099 21.3624 590.063 14.0296 581.818 13.2297L447.448 0.194357ZM1086.03 431.727C1089.68 439.166 1098.66 442.239 1106.1 438.593C1113.54 434.946 1116.62 425.96 1112.97 418.521L1086.03 431.727ZM434.419 24.6572C449.463 42.934 474.586 81.0463 521.375 116.908C568.556 153.07 637.546 187.063 742.018 200.993L745.982 171.256C646.454 157.985 582.444 125.917 539.625 93.0974C496.414 59.978 474.537 26.1903 457.581 5.59138L434.419 24.6572ZM742.018 200.993C939.862 227.372 1054.15 366.703 1086.03 431.727L1112.97 418.521C1077.85 346.879 956.138 199.277 745.982 171.256L742.018 200.993Z" fill="currentColor" class="fill-gray-800 dark:fill-white"/>
+        </svg>
+      </div>
+      <!-- End SVG Element -->
+    </div>
+  </div>
+</div>
+<!-- End Hero -->
+   
+   
+   
+     </main>
+     <!-- ========== END MAIN CONTENT ========== -->
+   
+   
+   
+   </div>
+   
+   </template>
+   
+   <script>
+   export default {
+     name: 'MarketingPage',
+   }
+   </script>
+   
+   <style>
+   /* You can add any custom styles here */
+   
+   </style>
+   
